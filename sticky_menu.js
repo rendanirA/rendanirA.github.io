755b7b3ce67b3ea204e685e7e0583d7e2c8dd99b
rendanirA <53425065+rendanirA@users.noreply.github.com>
2023-02-22 14:48:35 +0200

    added bold text

---
+++ sticky_menu.js
@@ -0,0 +1,11 @@
+var navbar =document.getElementById("navbar");
+var menu =document.getElementById("menu");
+
+window.onscroll=function(){
+    if(window.pageXOffset >=menu.offsetTop){
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
